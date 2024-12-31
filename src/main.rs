0964632ffaaf6600a073013f60e2e01213855dd2
Chris Cody <3807221+codeitlikecody@users.noreply.github.com>
2024-12-31 17:12:13 +1100

    tutorial 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use cursive::{self, views::TextView};
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+    siv.add_global_callback('q', |s| s.quit());
+    siv.add_layer(TextView::new("Hello cursive! Press <q> to quit."));
+    siv.run();
 }
