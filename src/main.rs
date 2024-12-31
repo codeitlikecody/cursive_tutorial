cd8a1c3b40ac596ff01bb3d9886ed75d3095b762
Chris Cody <3807221+codeitlikecody@users.noreply.github.com>
2024-12-31 18:00:20 +1100

    tutorial 3

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,75 @@
 use cursive::{
     self,
-    views::{Dialog, TextView},
+    reexports::log::info,
+    view::{Nameable, Resizable},
+    views::{Button, Dialog, DummyView, EditView, LinearLayout, SelectView, TextView},
     Cursive,
 };
 
 fn main() {
     let mut siv = cursive::default();
 
+    let select = SelectView::<String>::new()
+        .on_submit(on_submit)
+        .with_name("select")
+        .fixed_size((10, 5));
+
+    let buttons = LinearLayout::vertical()
+        .child(Button::new("Add new", add_name))
+        .child(Button::new("Delete", delete_name))
+        .child(DummyView)
+        .child(Button::new("Quit", Cursive::quit));
+
     siv.add_layer(
-        Dialog::text("This is a survey!\nPress <Next> when you're ready")
-            .title("Important Survey")
-            .button("Next", show_next),
+        Dialog::around(
+            LinearLayout::horizontal()
+                .child(select)
+                .child(DummyView)
+                .child(buttons),
+        )
+        .title("Select a profile"),
     );
 
     siv.run();
 }
 
-fn show_next(s: &mut Cursive) {
+fn on_submit(s: &mut Cursive, name: &str) {
     s.pop_layer();
     s.add_layer(
-        Dialog::text("Did yo do the thing?")
-            .title("Question 1")
-            .button("Yes!", |s| show_answer(s, "I knew it!, Well done!"))
-            .button("No!", |s| show_answer(s, "I knew you couldn't be trusted!"))
-            .button("Uh?", |s| s.add_layer(Dialog::info("Try again!"))),
+        Dialog::text(format!("Name: {}\nAwesome: yes", name))
+            .title(format!("{}'s info", name))
+            .button("Quit", Cursive::quit),
     );
 }
 
-fn show_answer(s: &mut Cursive, msg: &str) {
-    s.pop_layer();
+fn add_name(s: &mut Cursive) {
+    fn ok(s: &mut Cursive, name: &str) {
+        s.call_on_name("select", |view: &mut SelectView<String>| {
+            view.add_item_str(name);
+        });
+        s.pop_layer();
+    }
+
     s.add_layer(
-        Dialog::text(msg)
-            .title("Results")
-            .button("Finish", |s| s.quit()),
+        Dialog::around(EditView::new().with_name("name").fixed_width(10))
+            .title("Enter a new name")
+            .button("Ok", |s| {
+                let name = s
+                    .call_on_name("name", |view: &mut EditView| view.get_content())
+                    .unwrap();
+                ok(s, &name);
+            })
+            .button("Cancel", |s| {
+                s.pop_layer();
+            }),
     );
+}
+fn delete_name(s: &mut Cursive) {
+    let mut select = s.find_name::<SelectView<String>>("select").unwrap();
+    match select.selected_id() {
+        None => s.add_layer(Dialog::info("No names to remove")),
+        Some(focus) => {
+            select.remove_item(focus);
+        }
+    }
 }
